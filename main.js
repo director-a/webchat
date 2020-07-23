c81ea3420f39f5189395fc7bf14ddbc2ca5f97f0
director-a <62399714+director-a@users.noreply.github.com>
2020-07-23 13:51:54 +0300

    ф

--- main.js
+++ main.js
@@ -1,40 +1,49 @@
 var messages = document.getElementById('messages');
-var sendButton = document.getElementById('send-btn')
+var roomNameInput = document.getElementById('roomname-input');
+var sendButton = document.getElementById('send-btn');
 
 //по нажатию на кнопку отправить - отправить на сервер nickname:message 
 sendButton.addEventListener('click', sendUserMessage); 
 
-getMessagesFromServer();
-
+start();
+    
+//каждые 500 милисек забирать сообщение
+function start(){
+    getMessagesFromServer();
+    setInterval(getMessagesFromServer, 3000 );  
+}
+var lastMessage = [];
 //шаг 1:
 //получить сообщение с сервера
-async function getMessagesFromServer() {    
- var response = await fetch('https://fchatiavi.herokuapp.com/get/artem/?offset=0&limit=10000000');
+async function getMessagesFromServer() {
+    
+    //получаем название комнаты
+    var roomname = roomNameInput.value;
+    //получить асинхронный ответ
+ var response = await fetch(`https://fchatiavi.herokuapp.com/get/${roomname}/?offset=0&limit=10000000`);
+    //декодируем из строки в жс
  response = await response.json();
     
-    var allMessagesHTML = '';
-    for (var i = 0; i < response.length; i++){
-       var messageData = response[i];
-        //создать верстку меседжа   
-       var message = `
-        <div class="message">
-         <div class="message-nickname"> ${messageData.Name}</div>
-         <div class="message-text"> ${messageData.Message} </div>
-       </div>
-    `
-        
-     allMessagesHTML = allMessagesHTML + message;
-        
+    if(response == null) {
+     messages.innerHTML = 'No messages';
+        return;
     }
-     messages.innerHTML = allMessagesHTML;
     
+    //сформировать хтмл меседжей
+    var messagesHTML = fromMessagesHTML(response);
     
-   
+     //добавить в меседж врапер письма
+     messages.innerHTML = messagesHTML;
     
-    
+    //если сообщение больше чем прошлый раз проскролить вниз
+    if (lastMessages.length < response.length) {
+        scrollToEnd();
+    }
+    //запомнить сообщение
+   lastMessages = response;
 }
 
-//добавить в messages-wrapper письма
+
 async function sendUserMessage() {
 //получить что написал пользователь в поле nickname
 
@@ -51,8 +60,8 @@ var userMessage = document.getElementById('message-input').value;
     alert("Ты должен ввести сообщение!");
     return;
 }
-    
-     await fetch('https://fchatiavi.herokuapp.com/send/artem/',     {
+     var roomname = roomNameInput.value;
+     await fetch(`https://fchatiavi.herokuapp.com/send/${roomname}/`,     {
          method: 'POST',
          body: JSON.stringify({
              Name: userNickname,
@@ -61,7 +70,34 @@ var userMessage = document.getElementById('message-input').value;
      });
     
     getMessagesFromServer();
-    
-        
 }
+    
+    //сформировать html меседжей
+    function fromMessagesHTML(messages){
+        console.log(messages);
+    var allMessagesHTML = '';
+    for (var i = 0; i < messages.length; i++){
+       var messageData = messages[i];
+        //создать верстку меседжа   
+       var message = `
+        <div class="message">
+         <div class="message-nickname"> ${messageData.Name}</div>
+         <div class="message-text"> ${messageData.Message} </div>
+       </div>
+    `
+        
+     allMessagesHTML = allMessagesHTML + message;
+        
+    }
+    return allMessagesHTML;
+    }
+    //добавить в messages-wrapper письма
 
+     
+        
+
+
+function scrollToEnd(){
+    messages.scrollTop = messages.scrollHeight;
+    
+}
